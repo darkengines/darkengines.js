6983c6bd11b71b18461525b0e3fbb72d2254892e
Quicksort <Quicksort@302f0312-3c5d-0d4b-b714-136c201f073e>
2014-10-24 00:43:50 +0000

    git-svn-id: https://darkengines.com/svn/darkengines/darkengines.js@596 302f0312-3c5d-0d4b-b714-136c201f073e

---
+++ core.js/app.js
@@ -0,0 +1,27 @@
+﻿var WebSocketServer = require('websocket').server;
+var http = require('http');
+var url = require('url');
+
+var server = http.createServer(function (request, response) {
+
+});
+server.listen(1337, function () {});
+var wsServer = new WebSocketServer({
+    httpServer: server,
+    maxReceivedFrameSize: 64000000
+});
+
+wsServer.on('request', function (request) {
+    var connection = request.accept(null, request.origin);
+    connection.on('message', function (message) {
+        if (message.type === 'utf8') {
+            console.log(message);
+            wsServer.connections.forEach(function (c) {
+                c.send(message.utf8Data);
+            });
+        }
+    });
+    
+    connection.on('close', function (connection) {
+    });
+});
