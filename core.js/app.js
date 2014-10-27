14421db29fc70a8f399abb4ac0179b634b8b1b4d
Quicksort <Quicksort@302f0312-3c5d-0d4b-b714-136c201f073e>
2014-10-27 02:13:15 +0000

    git-svn-id: https://darkengines.com/svn/darkengines/darkengines.js@600 302f0312-3c5d-0d4b-b714-136c201f073e

--- core.js/app.js
+++ core.js/app.js
@@ -1,24 +1,26 @@
 ﻿var WebSocketServer = require('websocket').server;
 var http = require('http');
 var url = require('url');
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
 
 var server = http.createServer(function (request, response) {
 
 });
-server.listen(1337, function () {});
+server.listen(1337, function () { });
+var events = {};
 var wsServer = new WebSocketServer({
     httpServer: server,
     maxReceivedFrameSize: 64000000
 });
-
 wsServer.on('request', function (request) {
     var connection = request.accept(null, request.origin);
     connection.on('message', function (message) {
         if (message.type === 'utf8') {
-            console.log(message);
-            wsServer.connections.forEach(function (c) {
-                c.send(message.utf8Data);
-            });
+            message = JSON.parse(message.utf8Data);
+            if (server.events.hasOwnProperty(message.event)) {
+                events[message.event](wsServer, message.data);
+            }
         }
     });
     
